b1f54a458d5f47151a9b12d3cf851f1aa1b47504
Anzhelika <mrstonem@mail.ru>
2023-11-07 20:49:42 +0300

    app

---
+++ front/src/app/App.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router/AppRouter';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+
+function App() {
+    return (
+        <div className={classNames('app', {}, [])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+}
+
+export default App;
