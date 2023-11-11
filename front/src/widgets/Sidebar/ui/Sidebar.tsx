9ac4846105dee16cafa3a00a77e4de2d18a4ba18
Anzhelika <mrstonem@mail.ru>
2023-11-11 11:47:28 +0300

    sidebar

--- front/src/widgets/Sidebar/ui/Sidebar.tsx
+++ front/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'utils/classNames';
 import { memo } from 'react';
 import cls from './Sidebar.module.scss';
+import { Breadcrumb } from '../../Breadcrumb';
+import { Filters } from "../../Filters";
 
 interface SidebarProps {
     className?: string;
@@ -11,7 +13,11 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
         <aside
             className={classNames(cls.Sidebar, {}, [className])}
         >
-            <div>SIDEBAR</div>
+            <div className={cls.container}>
+                <div className={cls.appName}><h2>PaidEmail</h2></div>
+                <Breadcrumb />
+                <Filters />
+            </div>
         </aside>
     );
 });
