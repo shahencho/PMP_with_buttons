564c6f5df4d0a2ce29c08689f43d481e55ce16ef
Anzhelika <mrstonem@mail.ru>
2023-11-14 21:50:57 +0300

    route

--- front/src/app/router/routeConfig.tsx
+++ front/src/app/router/routeConfig.tsx
@@ -4,12 +4,14 @@ import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     INFLUENCERS = 'influencers',
+    INFLUENCER = 'influencer_details',
     // last
     NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.INFLUENCERS]: '/influencers',
+    [AppRoutes.INFLUENCER]: '/influencers/',
     // последний
     [AppRoutes.NOT_FOUND]: '*',
 };
@@ -17,6 +19,10 @@ export const RoutePath: Record<AppRoutes, string> = {
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.INFLUENCERS]: {
         path: RoutePath.influencers,
+        element: <InfluencersPage />,
+    },
+    [AppRoutes.INFLUENCER]: {
+        path: `${RoutePath.influencer_details}:id`,
         element: <InfluencersPage />,
     },
     // last
